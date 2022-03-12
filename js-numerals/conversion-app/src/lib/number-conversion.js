9ec10e61620e7ee5b2ee5ecb2655cdcc58bfa01e
n0rbertz <norbz123@gmail.com>
2022-03-12 21:08:05 +0100

    implemented conversion of numbers below 1000, added 3 tests

--- js-numerals/conversion-app/src/lib/number-conversion.js
+++ js-numerals/conversion-app/src/lib/number-conversion.js
@@ -1,5 +1,38 @@
 function convertNumberToWords(number) {
-    return "two"
+    if (number == 0) {
+        return "zero";
+    }
+    let numberStr = number.toString();
+    const ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "thirteen",
+                  "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    let numbers = [0, 0, 0];
+    let difference = 3 - numberStr.length;
+    for (let i = 0; i < numberStr.length; i++) {        
+        numbers[i + difference] = numberStr[i]; 
+    }
+    let word = "";
+    if (numbers[0] != 0) {
+        word = word + ones[numbers[0]] + " hundred";
+    }
+    if (numbers[1] != 0) {
+        if (word != "") {
+            word += " "
+        }
+        word += tens[numbers[1]];
+    }
+    if (numbers[2] != 0) {
+        if (numbers[1] != 0) {
+            word = word + "-" + ones[numbers[2]]
+        }
+        else {
+            if (word != "") {
+                word += " ";
+            }
+            word += ones[numbers[2]];
+        }
+    }
+    return word;
 }
 
 export default convertNumberToWords;
--- js-numerals/conversion-app/src/lib/number-conversion.test.js
+++ js-numerals/conversion-app/src/lib/number-conversion.test.js
@@ -3,3 +3,15 @@ import convertNumberToWords from './number-conversion'
 test("returns something", () => {
     expect(convertNumberToWords(2)).toBeDefined();
 })
+
+test("converts 456 to four hundred fifty-six", () => {
+    expect(convertNumberToWords(456)).toBe("four hundred fifty-six");
+})
+
+test("converts 23 to twenty-three", () => {
+    expect(convertNumberToWords(23)).toBe("twenty-three");
+})
+
+test("converts 8 to eight", () => {
+    expect(convertNumberToWords(8)).toBe("eight");
+})

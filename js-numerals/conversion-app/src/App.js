ac1c6f28cae1e1d744d4693cbffa8ecf650f4760
n0rbertz <norbz123@gmail.com>
2022-03-15 00:37:49 +0100

    create Button component

--- js-numerals/conversion-app/src/App.js
+++ js-numerals/conversion-app/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Button from './components/Button'
+import {useState} from 'react'
 
 function App() {
+  const [conversionType, setConversionType] = useState("standard");
   return (
     <div className="container">
       <Header></Header>
+      <Button title={"Standard"} color={"white"}></Button>
+      <Button title={"British"} color={"white"}></Button>
     </div>
   );
 }
---
+++ js-numerals/conversion-app/src/components/Button.js
@@ -0,0 +1,7 @@
+const Button = ({title, color}) => {
+    return (
+        <button backgroundColor={color}>{title}</button>
+    )
+}
+
+export default Button;

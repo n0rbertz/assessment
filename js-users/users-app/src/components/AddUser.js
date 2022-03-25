f2306c79679a8dd83258b151902b239310b03649
n0rbertz <norbz123@gmail.com>
2022-03-25 21:41:07 +0100

    Added form to add new user

--- js-users/users-app/src/components/AddUser.js
+++ js-users/users-app/src/components/AddUser.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const AddUser = () => {
   return (
-    <div>AddUser</div>
+    <div>
+      <form>
+        <label>First name:
+          <input type={"text"}/>
+        </label>
+        <br></br>
+        <label>Last name:
+          <input type={"text"}/>
+        </label>
+        <br></br>
+        <button type="submit">Add User</button>
+      </form>
+    </div>
   )
 }
  export default AddUser

d30348d7194480ce0c6b959f54987eb3019391f0
n0rbertz <norbz123@gmail.com>
2022-03-25 22:51:21 +0100

    Added new user fetch, state of firstName and lastName

--- js-users/users-app/src/components/AddUser.js
+++ js-users/users-app/src/components/AddUser.js
@@ -1,18 +1,52 @@
 import React from 'react'
+import {useState} from 'react'
 
 const AddUser = () => {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value)
+    console.log(firstName)
+  }
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value)
+    console.log(lastName)
+  }
+
+  const addNewUser = async (event) => {
+    event.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        status: "active"
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    await fetch('https://assessment-users-backend.herokuapp.com/users', requestOptions)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+  }
+
   return (
     <div>
       <form>
         <label>First name:
-          <input type={"text"}/>
+          <input type={"text"} onChange={handleFirstNameChange}/>
         </label>
         <br></br>
         <label>Last name:
-          <input type={"text"}/>
+          <input type={"text"} onChange={handleLastNameChange}/>
         </label>
         <br></br>
-        <button type="submit">Add User</button>
+        <button type="submit" onClick={addNewUser}>Add User</button>
       </form>
     </div>
   )

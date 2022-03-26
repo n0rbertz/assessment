2f46c9d1356fa75b57196c29d0dfcd1f35dfb1db
n0rbertz <norbz123@gmail.com>
2022-03-26 18:25:21 +0100

    Add form to /edit route

--- js-users/users-app/src/components/EditUser.js
+++ js-users/users-app/src/components/EditUser.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+
 const EditUser = () => {
+
+  
+
   return (
-    <div>EditUser</div>
+    <div>
+      <form>
+        <label>First name:
+          <input type={"text"}/>
+        </label>
+        <br></br>
+        <label>Last name:
+          <input type={"text"}/>
+        </label>
+        <br></br>
+        <button type="submit">Add User</button>
+      </form>
+    </div>
   )
 }
 

e59bda8403fc2742e3a439b76c5772d8b6a02dab
n0rbertz <norbz123@gmail.com>
2022-03-26 18:41:49 +0100

    Added onChange events which store the current value of input field

--- js-users/users-app/src/components/EditUser.js
+++ js-users/users-app/src/components/EditUser.js
@@ -1,19 +1,37 @@
 import React from 'react'
+import {useState} from 'react'
+import {useParams} from 'react-router-dom'
 
 
 const EditUser = () => {
 
-  
+  const {id} = useParams()
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value)
+    console.log(firstName)
+  }
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value)
+    console.log(lastName)
+  }
+
+  const editUser = async (event) => {
+    event.preventDefault();
+  }
 
   return (
     <div>
       <form>
         <label>First name:
-          <input type={"text"}/>
+          <input type={"text"} onChange={handleFirstNameChange}/>
         </label>
         <br></br>
         <label>Last name:
-          <input type={"text"}/>
+          <input type={"text"} onChange={handleLastNameChange}/>
         </label>
         <br></br>
         <button type="submit">Add User</button>

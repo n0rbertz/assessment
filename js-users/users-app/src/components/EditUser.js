2b3371bb8b20bfaac0adfb7ac7253e86d58ea7b2
n0rbertz <norbz123@gmail.com>
2022-03-27 21:48:12 +0200

    Added feature of displaying errors when trying to edit user

--- js-users/users-app/src/components/EditUser.js
+++ js-users/users-app/src/components/EditUser.js
@@ -6,21 +6,23 @@ import {useParams} from 'react-router-dom'
 const EditUser = () => {
 
   const {id} = useParams()
+  const [firstNameErrorMessage, setFirstNameErrorMessage] = useState("")
+  const [lastNameErrorMessage, setLastNameErrorMessage] = useState("")
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
 
   const handleFirstNameChange = (event) => {
     setFirstName(event.target.value)
-    console.log(firstName)
   }
 
   const handleLastNameChange = (event) => {
     setLastName(event.target.value)
-    console.log(lastName)
   }
 
   const editUser = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
+    setFirstNameErrorMessage("")
+    setLastNameErrorMessage("")
     const requestOptions = {
       method: 'PUT',
       body: JSON.stringify({
@@ -30,10 +32,24 @@ const EditUser = () => {
       headers: {
           'Content-Type': 'application/json'
       }
+    }
+    await fetch(`https://assessment-users-backend.herokuapp.com/users/${id}`, requestOptions)
+    .then((response) => response.text())
+    .then((data) => data === "" ? {} : handleErrorMessages(JSON.parse(data)))
+    .catch((error) => console.log(error))
   }
-  await fetch(`https://assessment-users-backend.herokuapp.com/users/${id}`, requestOptions)
-  .then((response) => console.log(response))
-  .catch((error) => console.log(error))
+
+  const handleErrorMessages = (message) => {
+    for (let [key, value] of Object.entries(message)) {
+      const fieldOfError = key
+      const errorMessage= value
+      if (fieldOfError == "first_name") {
+        setFirstNameErrorMessage(`First name ${errorMessage}`)
+      }
+      if (fieldOfError == "last_name") {
+        setLastNameErrorMessage(`Last name ${errorMessage}`)
+      }
+    }
   }
 
   return (
@@ -42,10 +58,12 @@ const EditUser = () => {
         <label>First name:
           <input type={"text"} onChange={handleFirstNameChange}/>
         </label>
+        {firstNameErrorMessage}
         <br></br>
         <label>Last name:
           <input type={"text"} onChange={handleLastNameChange}/>
         </label>
+        {lastNameErrorMessage}
         <br></br>
         <button type="submit" onClick={editUser}>Edit User</button>
       </form>
